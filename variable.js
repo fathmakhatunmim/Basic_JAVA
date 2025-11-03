c5b90e382ab46119218f03ab117cd45850c4bc15
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-11-03 10:34:45 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,33 @@
+//variable = vary + able
+// 5 things todeclear a variable
+//number
+var weight = 59;
+var money = 500;
+var age;
+
+console.log(money);
+console.log(age);
+
+
+//string 
+var name = "mim";
+console.log(name);
+
+
+//boolean
+
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+
+
+console.log(typeof isRich);
+
+
+
+
+
+
+
